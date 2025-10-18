a3f86c6239498e6345d070892038d491fe0c7933
Josue Julianz Noha Valdiva <65608253+JosueNoha@users.noreply.github.com>
2025-10-17 19:44:55 -0500

    frames

--- csi_py.py
+++ csi_py.py
@@ -1,7 +1,58 @@
 import comtypes.client
 import pandas as pd
+import numpy as np
 import psutil
 import os
+
+eFramePropType = {
+    1: 'I',
+    2: 'Channel',
+    3: 'T',
+    4: 'Angle',
+    5: 'DblAngle',
+    6: 'Box',
+    7: 'Pipe',
+    8: 'Rectangular',
+    9: 'Circle',
+    10: 'General',
+    11: 'DbChannel',
+    12: 'Auto',
+    13: 'SD',
+    14: 'Variable',
+    15: 'Joist',
+    16: 'Bridge',
+    17: 'Cold_C',
+    18: 'Cold_2C',
+    19: 'Cold_Z',
+    20: 'Cold_L',
+    21: 'Cold_2L',
+    22: 'Cold_Hat',
+    23: 'BuiltupICoverplate',
+    24: 'PCCGirderI',
+    25: 'PCCGirderU',
+    26: 'BuiltupIHybrid',
+    27: 'BuiltupUHybrid',
+    28: 'Concrete_L',
+    29: 'FilledTube',
+    30: 'FilledPipe',
+    31: 'EncasedRectangle',
+    32: 'EncasedCircle',
+    33: 'BucklingRestrainedBrace',
+    34: 'CoreBrace_BRB',
+    35: 'ConcreteTee',
+    36: 'ConcreteBox',
+    37: 'ConcretePipe',
+    38: 'ConcreteCross',
+    39: 'SteelPlate',
+    40: 'SteelRod',
+    41: 'PCCGirderSuperT',
+    42: 'Cold_Box',
+    43: 'Cold_I',
+    44: 'Cold_Pipe',
+    45: 'Cold_T',
+    46: 'Trapezoidal',
+    47: 'PCCGirderBox'
+}
 
 def validate_programs(program):
     # Validar y estandarizar el programa
@@ -201,7 +252,7 @@ class CSIHandler:
         return list(load_cases)
     
     def get_combos(self):
-        load_combos = self.SapModel.RespCombo.GetNameList()[1]
+        load_combos = self.model.RespCombo.GetNameList()[1]
         load_combos = [i for i in load_combos if i[0] != '~']
         return list(load_combos)
     
@@ -267,12 +318,132 @@ class CSIHandler:
         
         return data_forces
 
+    def get_frame_list(self):
+        """Obtiene lista de todos los frames"""
+        return list(self.model.FrameObj.GetNameList()[1])
+    
+    def get_frame_section(self, frame_name):
+        """Obtiene sección asignada a un frame"""
+        frame_name = f'{frame_name}'
+        return self.model.FrameObj.GetSection(frame_name)[0]
+    
+    def get_frame_points(self, frame_name):
+        """Obtiene puntos inicial y final de un frame"""
+        frame_name = f'{frame_name}'
+        result = self.model.FrameObj.GetPoints(frame_name)
+        return {'point_i': result[0], 'point_j': result[1]}
+    
+    def get_frame_coordinates(self,frame_name):
+        """Obtiene coordenadas de los puntos inicial y final"""
+        points = self.get_frame_points(frame_name)
+        coord_i = self.model.PointObj.GetCoordCartesian(points['point_i'])[:-1]
+        coord_j = self.model.PointObj.GetCoordCartesian(points['point_j'])[:-1]
+        
+        return {'coord_i': coord_i, 'coord_j': coord_j}
+    
+    def get_frame_length(self, frame_name):
+        """Obtiene longitud del frame"""
+        coords = self.get_frame_coordinates(frame_name)
+        coord_i = coords['coord_i']
+        coord_j = coords['coord_j']
+        
+        import numpy as np
+        length = np.sqrt(sum((coord_j[i] - coord_i[i])**2 for i in range(3)))
+        return length
+    
+    def extract_frame_forces(self, frame_name=None, cases_and_combos=None):
+        """
+        Extrae fuerzas en frames usando API nativa
+        
+        Parameters:
+        -----------
+        frame_name : str, optional
+            Nombre del frame. Si None, extrae todos.
+        cases_and_combos : list, optional
+            Lista de casos/combos. Si None, usa todos.
+        
+        Returns:
+        --------
+        DataFrame con P, V2, V3, T, M2, M3 para cada frame
+        """
+        
+        frames = [frame_name] if frame_name else self.get_frame_list()
+        cases_and_combos = (
+        [cases_and_combos] if isinstance(cases_and_combos, str)
+        else list(cases_and_combos) if cases_and_combos
+        else self.get_cases_and_combos()
+        )
+
+        data = {
+        'Frame': [], 'Station': [], 'OutputCase': [], 
+        'StepType': [], 'StepNumber': [],
+        'P': [], 'V2': [], 'V3': [], 'T': [], 'M2': [], 'M3': []
+        }
+        
+        self.model.Results.Setup.DeselectAllCasesAndCombosForOutput()
+        for case in cases_and_combos :
+            self.model.Results.Setup.SetCaseSelectedForOutput(case)
+            self.model.Results.Setup.SetComboSelectedForOutput(case)
+            
+        frame_force = self.model.Results.FrameForce
+        for frame in frames:
+            res = frame_force(frame, 0)
+            if res[0] == 0:
+                continue
+
+            data['Frame'].extend(res[1])
+            data['Station'].extend(res[2])
+            data['OutputCase'].extend(res[5])
+            data['StepType'].extend(res[6])
+            data['StepNumber'].extend(res[7])
+            data['P'].extend(res[8])
+            data['V2'].extend(res[9])
+            data['V3'].extend(res[10])
+            data['T'].extend(res[11])
+            data['M2'].extend(res[12])
+            data['M3'].extend(res[13])
+                
+        return pd.DataFrame(data)
+
+    def get_frame_section_properties(self, section_name):
+        """Obtiene propiedades de una sección"""
+        result = self.model.PropFrame.GetSectProps(section_name)
+        return {
+            'SectionName': section_name,
+            'Area': result[0],
+            'As2': result[1],
+            'As3': result[2],
+            'Torsion': result[3],
+            'I22': result[4],
+            'I33': result[5],
+            'S22': result[6],
+            'S33': result[7],
+            'Z22': result[8],
+            'Z33': result[9],
+            'R22': result[10],
+            'R33': result[11]
+        }
+        
+    def get_frame_section_dimensions(self,get_properties=False):
+        '''Dimensiones de las secciones'''
+        columns = ['SectionName','PropType','t3','t2','tf','tw','t2b','tfb','Area']
+        data = self.model.PropFrame.GetAllFrameProperties_2()[1:-1]
+        data = pd.DataFrame(np.array(data).T,columns=columns)
+        data['SectionType'] = data['PropType'].astype(int).map(eFramePropType)
+        data = data[['SectionName', 'SectionType', 't3', 't2', 'tf', 'tw', 't2b', 'tfb', 'Area']]
+        if get_properties:
+            df_props = pd.DataFrame(data['SectionName'].apply(self.get_frame_section_properties).tolist())
+            data = data.merge(df_props,on='SectionName',how='left')
+        return data
 
         
-    
-    
 
 if __name__ == '__main__':
+    import time
     etabs_model = CSIHandler('Etabs')
-    etabs_model.connect_open_instance(0)
+    etabs_model.connect_open_instance()
+    to = time.time()
+    #print(etabs_model.get_frame_section_properties('VIGA 30X60'))
+    print(etabs_model.get_frame_section_dimensions())
+    print(time.time()-to)
     
